a0184b03a44120230ee8abb19ceb9a3c952940f9
first-p <98608650+first-p@users.noreply.github.com>
2022-12-07 12:34:48 -0700

    main.h updated

--- main.h
+++ main.h
@@ -16,13 +16,6 @@ libraries for main.cpp
 
 #include "graph.h"
 
-#define BUFFERSIZE 8
-#define MAXID 100
-#define BASE 5
-#define OFFSET 20
-#define MULTIPLIER 1.5
-#define MULTIPLIER2 2.4
-
 using std::cout;
 using std::endl;
 using std::string;
