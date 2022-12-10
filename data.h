dd1bc62e0af3f7c28411855c4c2a75061e84910d
first-p <98608650+first-p@users.noreply.github.com>
2022-12-10 16:38:23 -0700

    no change

--- data.h
+++ data.h
@@ -1,13 +1,3 @@
-/************************************
-Name: Fred Butoma
-Final Project
-
-Purpose: data.h is a struct used by the
-linked list in the form of nodes, and uses 
-linked lists as a part of a node in of
-itself
-************************************/
-
 #include <vector>
 #include <string>
 
